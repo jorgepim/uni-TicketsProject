36f7f716ed171d0e7d79104cac18827f102f25cf
Jorge <willygetta4@gmail.com>
2025-05-08 12:10:52 -0600

    start login

---
+++ TicketsApp/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketsApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
