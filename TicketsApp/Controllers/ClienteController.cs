47f6a2c9582a34bec25378c7d7a449225328886d
Jorge <willygetta4@gmail.com>
2025-05-15 20:12:26 -0600

    register

---
+++ TicketsApp/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketsApp.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Tickets()
+        {
+            return View();
+        }
+    }
+}
