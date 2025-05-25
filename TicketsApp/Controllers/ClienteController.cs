45188f05d6872724f231f8f901956299d0853be0
Rod <oscar.aguilar1@catolica.edu.sv>
2025-05-24 23:34:27 -0600

    layoutadmin

--- TicketsApp/Controllers/ClienteController.cs
+++ TicketsApp/Controllers/ClienteController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TicketsApp.Controllers
 {
-    [Authorize(Roles = "Cliente")]
+    //[Authorize(Roles = "Cliente")]
     public class ClienteController : Controller
     {
         public IActionResult Tickets()
